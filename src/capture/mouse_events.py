0f858fa10f4ecbd6330e3758c0e1bcafa71283d7
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2018-08-24 11:06:06 -0700

    Add drag and drop rectangle

--- src/capture/mouse_events.py
+++ src/capture/mouse_events.py
@@ -1,12 +1,22 @@
 import numpy as np
 import cv2 as cv
 
-events = [i for i in dir(cv) if 'EVENT' in i]
-# print(events)
+drawing = False
+sPoint = (-1, -1)
+
 
 def draw_circle(event, x, y, flags, param):
-    if event == cv.EVENT_MOUSEWHEEL:
-        cv.circle(img, (x, y), 100, (255, 0, 0), -1)
+    global sPoint, drawing
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        sPoint = (x, y)
+        cv.circle(img, (x, y), 10, (255, 0, 0), -1)
+    # elif drawing and event == cv.EVENT_MOUSEMOVE:
+    #     cv.rectangle(img, sPoint, (x, y), (255, 255, 255), 3)
+    elif drawing and event ==cv.EVENT_LBUTTONUP:
+        drawing = False
+        cv.rectangle(img, sPoint, (x, y), (255, 255, 255), 3)
+        cv.circle(img, (x, y), 10, (0, 255, 0), -1)
     elif event == cv.EVENT_RBUTTONDOWN:
         cv.rectangle(img, (x, y), (x + 50, y + 75), (100, 255, 100), -1)
 
