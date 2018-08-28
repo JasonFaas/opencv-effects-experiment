ede32be89629adb9e194c70a1d27542bacbd420b
JasonFaas <5844854+JasonFaas@users.noreply.github.com>
2018-08-28 08:32:04 -0700

    Cleanup all-cameras. Add trackbar

--- src/capture/all-cameras.py
+++ src/capture/all-cameras.py
@@ -3,29 +3,28 @@ import numpy as np
 import datetime
 
 
-camera_list = [1, 2]
-window_name = "Camera_%s" % str(0)
-cv.namedWindow(window_name)
-#TODO: Initialize cameras with empty so everything can be done in loop
-cameras = [[cv.VideoCapture(0), window_name]]
+camera_list = [0,1,2]
+cameras = {}
 
 #initialize cameras
 for cam_int in camera_list:
     capture = cv.VideoCapture(cam_int)
     window_name = "Camera %s" % str(cam_int)
     cv.namedWindow(window_name)
-    cameras = np.append(cameras, [[capture, window_name]], axis = 0)
+    cameras[window_name] = capture
+    # cameras = np.append(cameras, [[capture, window_name]], axis = 0)
 
 display_video = True
 recording = False
 
 while display_video:
-    for cam_int in cameras:
-        ret, frame = cam_int[0].read()
+    for win_name, vid_cap in cameras.items():
+        ret, frame = vid_cap.read()
         if not ret:
             continue
-        cv.imshow(cam_int[1], frame)
+        cv.imshow(win_name, frame)
         key_press = cv.waitKey(1) & 0xFF
+
 
         if key_press == 27:
             # ESC pressed
@@ -36,13 +35,13 @@ while display_video:
             #TODO: Either add functionality for frame specific frame saving or save all 3
             print("saving image")
             datetime_now = str(datetime.datetime.now()).replace(' ', '_')
-            img_name = "{}-{}.png".format(str(cam_int[1]), datetime_now)
+            img_name = "{}-{}.png".format(win_name, datetime_now)
 
             cv.imwrite(img_name, frame)
             print("{} written!".format(img_name))
         #TODO: Add recording option
 
-for cam_int in cameras:
-    cam_int[0].release()
+for win_name, vid_cap in cameras.items():
+    vid_cap.release()
 
 cv.destroyAllWindows()
